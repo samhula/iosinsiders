c1caa24e462c4a2da6552ca047aa8b3f103d5a6e
samhula <samhula99@gmail.com>
2023-02-27 13:43:27 +0000

    search bar displayed

--- app/core/functions.php
+++ app/core/functions.php
@@ -2,25 +2,23 @@
 
 	// Echoing out the navigation bar
 	function setNavBar($navBar){
-		echo '<li class="flex-element-1"><a href="'.URL.'/public/home">Home</a></li>';
-
 		foreach ($navBar as $category) {
 			echo '<li class="flex-element-1"><a href="'.URL.'/public/'.strtolower($category).'">'.$category.'</a></li>';
 		}
 
-		/*
-			<li>
-				<form method="GET" action="#">
-				<input type="text" placeholder="Search.." name="search">
-	   			<button type="submit"></button>
-				</form>
-			</li>
-			<a href="#" class="toggle-hamburger">
-				<span class="hamburger-bar"></span>
-				<span class="hamburger-bar"></span>
-				<span class="hamburger-bar"></span>
-			</a> 
-		*/
+		
+			echo '<li>';
+			echo '	<form method="GET" action="#">';
+			echo '	<input type="text" placeholder="Search.." name="search">';
+	   		echo '	<button type="submit"></button>';
+			echo '	</form>';
+			echo '</li>';
+			echo '<a href="#" class="toggle-hamburger">';
+			echo '	<span class="hamburger-bar"></span>';
+			echo '	<span class="hamburger-bar"></span>';
+			echo '	<span class="hamburger-bar"></span>';
+			echo '</a> ';
+		
 	}
 
 	// Echoing out the footer
