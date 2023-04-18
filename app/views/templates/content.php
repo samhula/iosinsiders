a7535f30fecd2fc05ca5bb85122af8389556b993
samhula <samhula99@gmail.com>
2023-04-18 22:32:57 +0100

    Article UI

--- app/views/templates/content.php
+++ app/views/templates/content.php
@@ -5,8 +5,132 @@ require_once 'nav.php';
 ?>
 <body>
 	<div class="content-container">
-		<div class="flex-element-6 element">
-			<h2>Hello</h2>
+		<div class="article flex-element-1">
+			<div class="wrapper">
+				<section class="content flex-element-2">
+					<h2 class="article-title">What is the best PC under £1000?</h2>
+					<div class="info-container">
+						By <span class="author">Author</span><span class="bullet">&#x2022;</span>Updated on</li> <span class="date-of-publish"> 18th April 2023</span>
+					</div>
+					<img class="main-article-image" src="https://ichef.bbci.co.uk/news/976/cpsprodpb/CA49/production/_129358715_gettyimages-1333127675-170667a.jpg.webp">
+					<!-- <div class="description-container">
+						This is the description
+					</div> -->
+					<h3 id="Heading-1">Heading 1</h3>
+					<p class="article-paragraph">Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. </p>
+					<h3>Heading X</h3>
+					<p class="article-paragraph">Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. Here is some written article content. </p>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					<h3>Heading X</h3>
+					
+				</section>
+				<section class="element table-of-contents flex-element-1">
+					<div class="flex-element-1">
+					<div class="bold table-of-contents-title">Table of Contents</div>
+					<ol>
+						<li><a href="#Heading-1">This is the first heading</a></li>
+						<li>This is the second heading</li>
+						<li>This is the third heading</li>
+					</ol>
+					</div>
+				</section>
+				<!-- <section class="element">
+					
+				</section> -->
+			</div>
 		</div>
 	</div>
 </body>
